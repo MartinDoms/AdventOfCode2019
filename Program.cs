307539c71c8f5549f45397220870298d09d9fd46
Martin Doms <martin.doms@arlo.co>
2019-12-05 10:03:09 +1300

    Day 3 first cut (broken)

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ public class Program {
     public static void Main() {
         Console.WriteLine($"Day 1 Problem 1\t{Day1.Problem1()}");
         Console.WriteLine($"Day 1 Problem 2\t{Day1.Problem2()}");
+
         Console.WriteLine($"Day 2 Problem 1\t{Day2.Problem1()}");
         Console.WriteLine($"Day 2 Problem 2\t{Day2.Problem2()}");
+
+        //Console.WriteLine($"Day 3 Problem 1\t{Day3.Problem1()}");
+        Console.WriteLine($"Day 3 Problem 2\t{Day3.Problem2()}");
     }
 }
